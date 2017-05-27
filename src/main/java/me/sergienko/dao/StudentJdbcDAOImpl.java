98fad2b3e65aaaf6376fb4a2df8fae87d8d822d8
Roman Sergienko <r-sergienko@ya.ru>
2017-05-27 20:30:29 +0400

    Implement getStudent()

--- src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
+++ src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
@@ -1,9 +1,81 @@
 package me.sergienko.dao;
 
-/**
- * Created by Роман on 25.05.2017.
- */
-public class StudentJdbcDAOImpl {
-//TODO    implement method from StudentDAO
-//TODO  create file in root path package, create this class object and write to console student (use overridi "toString" )
+import me.sergienko.model.Student;
+
+
+import java.sql.*;
+import java.util.List;
+
+
+public class StudentJdbcDAOImpl implements StudentDAO{
+
+    public Integer createStudent(Student student) {
+
+
+        return null;
+    }
+
+
+    public Student getStudent(Integer id) {
+
+        Student st =  new Student();
+        Connection connection = null;
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String name = "postgres";
+        String password = "postgres";
+        String select = "SELECT * FROM students WHERE id="+id;
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection(url, name, password);
+
+            Statement  statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(select);
+
+
+            st.setGroup_id(resultSet.getInt("group_id"));
+            st.setName(resultSet.getString("name"));
+            st.setSur_name(resultSet.getString("sur_name"));
+            st.setRating_ege(resultSet.getDouble("exam_result"));
+            st.setEnrolment_date(resultSet.getDate("enrolment_date"));
+
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return st;
+    }
+
+
+    public void deleteStudent(Integer id) {
+
+
+    }
+
+
+    public void updateStudent(Student student) {
+
+    }
+
+
+    public List<Student> listStudents() {
+        return null;
+    }
+
+
+
+
+
+
 }
