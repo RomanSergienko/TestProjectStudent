8cfc65691511a6552ec6b304ae0543c77e5b632e
Roman Sergienko <r-sergienko@ya.ru>
2017-05-29 22:22:24 +0400

    Work to next day)

--- src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
+++ src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
@@ -4,11 +4,15 @@ import me.sergienko.model.Student;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
+//TODO replace all statement on prepared statement
+//TODO close statements
+//TODO use try with resources
+//TODO read naming convention
+//TODO learn idea hotkeys
 
-public class StudentJdbcDAOImpl  implements StudentDAO{
+public class StudentJdbcDAOImpl implements StudentDAO {
 
     public Integer createStudent(Student student) {
         Connection connection = null;
@@ -17,15 +21,16 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
 
 
         try {
-            connection = new GetterConnect().getConnection();
+            connection = new ConnectionGetter().getConnection();
             PreparedStatement st = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
-            st.setInt(1, student.getGroup_id());
+            st.setInt(1, student.getGroupId());
             st.setString(2, student.getName());
-            st.setString(3, student.getSur_name());
-            st.setDouble(4, student.getRating_ege());
-            st.setDate(5, (Date) student.getEnrolment_date());
+            st.setString(3, student.getSurName());
+            st.setDouble(4, student.getRatingEge());
+            st.setDate(5, (Date) student.getEnrolmentDate());
 
             st.executeUpdate();
+            st.close();
 
             ResultSet genKeys = st.getGeneratedKeys();
             if (genKeys.next()) {
@@ -51,23 +56,17 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
     public Student getStudent(Integer id) {
 
         Connection connection = null;
-        Student st =  new Student();
-        String select = "SELECT * FROM students WHERE id="+id;
+        Student st = null;
+        String select = "SELECT * FROM students WHERE id=" + id;
 
         try {
-            connection = new GetterConnect().getConnection();
-            Statement  statement = connection.createStatement();
+            connection = new ConnectionGetter().getConnection();
+            Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(select);
 
-            while (resultSet.next()){
-                st.setId(resultSet.getInt("id"));
-                st.setGroup_id(resultSet.getInt("group_id"));
-                st.setName(resultSet.getString("name"));
-                st.setSur_name(resultSet.getString("sur_name"));
-                st.setRating_ege(resultSet.getDouble("exam_result"));
-                st.setEnrolment_date(resultSet.getDate("enrolment_date"));
+            while (resultSet.next()) {
+                st = createStudentFromResultSet(resultSet);
             }
-
 
 
         } catch (Exception ex) {
@@ -84,14 +83,25 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
         return st;
     }
 
+    private Student createStudentFromResultSet(ResultSet resultSet) throws SQLException {
+        Student st = new Student();
+        st.setId(resultSet.getInt("id"));
+        st.setGroupId(resultSet.getInt("group_id"));
+        st.setName(resultSet.getString("name"));
+        st.setSurName(resultSet.getString("sur_name"));
+        st.setRatingEge(resultSet.getDouble("exam_result"));
+        st.setEnrolmentDate(resultSet.getDate("enrolment_date"));
+        return st;
+    }
+
 
     public void deleteStudent(Integer id) {
         Connection connection = null;
         String delete = "DELETE FROM students WHERE id=" + id;
 
         try {
-            connection = new GetterConnect().getConnection();
-            Statement  statement = connection.createStatement();
+            connection = new ConnectionGetter().getConnection();
+            Statement statement = connection.createStatement();
             statement.execute(delete);
 
         } catch (Exception ex) {
@@ -112,15 +122,15 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
     public void updateStudent(Student student) {
         Connection connection = null;
         String update = "UPDATE students SET group_id="
-                + student.getGroup_id() + ", name='"
+                + student.getGroupId() + ", name='"
                 + student.getName() + "', sur_name='"
-                + student.getSur_name() + "', exam_result="
-                + student.getRating_ege() + ", enrolment_date='"
-                + student.getEnrolment_date() + "' WHERE id="
+                + student.getSurName() + "', exam_result="
+                + student.getRatingEge() + ", enrolment_date='"
+                + student.getEnrolmentDate() + "' WHERE id="
                 + student.getId();
         try {
-            connection = new GetterConnect().getConnection();
-            Statement  statement = connection.createStatement();
+            connection = new ConnectionGetter().getConnection();
+            Statement statement = connection.createStatement();
             statement.execute(update);
 
         } catch (Exception ex) {
@@ -139,23 +149,17 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
 
 
     public List<Student> listStudents() {
-        List<Student> studentList  = new ArrayList<>();
+        List<Student> studentList = new ArrayList<>();
         Connection connection = null;
         ResultSet resultSet;
         String select = "SELECT * FROM students";
 
         try {
-            connection = new GetterConnect().getConnection();
-            Statement  statement = connection.createStatement();
+            connection = new ConnectionGetter().getConnection();
+            Statement statement = connection.createStatement();
             resultSet = statement.executeQuery(select);
-            while(resultSet.next()) {
-                Student st = new Student();
-                st.setId(resultSet.getInt("id"));
-                st.setGroup_id(resultSet.getInt("group_id"));
-                st.setName(resultSet.getString("name"));
-                st.setSur_name(resultSet.getString("sur_name"));
-                st.setRating_ege(resultSet.getDouble("exam_result"));
-                st.setEnrolment_date(resultSet.getDate("enrolment_date"));
+            while (resultSet.next()) {
+                Student st = createStudentFromResultSet(resultSet);
                 studentList.add(st);
             }
 
@@ -170,19 +174,15 @@ public class StudentJdbcDAOImpl  implements StudentDAO{
                 }
             }
         }
-       return studentList;
+        return studentList;
     }
 
     //for testing
     public static void main(String[] args) {
         StudentJdbcDAOImpl studentJdbcDAO = new StudentJdbcDAOImpl();
-        Student st = new Student();
-        st.setGroup_id(150);
-        st.setName("Лера");
-        st.setSur_name("Лысова");
-        st.setRating_ege(80.99);
-        st.setEnrolment_date(new Date(Calendar.getInstance().getTimeInMillis()));
-        System.out.println(studentJdbcDAO.createStudent(st));
+        for (Student s : studentJdbcDAO.listStudents()) {
+            System.out.println(s);
+        }
     }
 
 }
