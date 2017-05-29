53739b1152709711407326977b65f771146a84e0
Roman Sergienko <r-sergienko@ya.ru>
2017-05-29 21:47:07 +0400

    Change method name according naming convection, override toString

--- src/main/java/me/sergienko/model/Student.java
+++ src/main/java/me/sergienko/model/Student.java
@@ -19,43 +19,55 @@ public class Student {
         this.id = id;
     }
 
-    public double getRating_ege() {
-        return ratingEge;
-    }
-
-    public void setRating_ege(double rating_ege) {
-        this.ratingEge = rating_ege;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public String getSur_name() {
-        return surName;
-    }
-
-    public int getGroup_id() {
-        return groupId;
-    }
-
-    public Date getEnrolment_date() {
-        return enrolmentDate;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setSur_name(String sur_name) {
-        this.surName = sur_name;
+    public String getSurName() {
+        return surName;
     }
 
-    public void setGroup_id(int group_id) {
-        this.groupId = group_id;
+    public void setSurName(String surName) {
+        this.surName = surName;
     }
 
-    public void setEnrolment_date(Date enrolment_date) {
-        this.enrolmentDate = enrolment_date;
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public Date getEnrolmentDate() {
+        return enrolmentDate;
+    }
+
+    public void setEnrolmentDate(Date enrolmentDate) {
+        this.enrolmentDate = enrolmentDate;
+    }
+
+    public double getRatingEge() {
+        return ratingEge;
+    }
+
+    public void setRatingEge(double ratingEge) {
+        this.ratingEge = ratingEge;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", groupId=" + groupId +
+                ", enrolmentDate=" + enrolmentDate +
+                ", ratingEge=" + ratingEge +
+                '}';
     }
 }
