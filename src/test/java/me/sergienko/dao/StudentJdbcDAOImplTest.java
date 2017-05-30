65124ddbc917760f8ffe5f08b31b7daf24c75b87
Roman Sergienko <r-sergienko@ya.ru>
2017-05-30 22:12:01 +0400

    Add  dir and class for test

---
+++ src/test/java/me/sergienko/dao/StudentJdbcDAOImplTest.java
@@ -0,0 +1,36 @@
+package me.sergienko.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Роман on 30.05.2017.
+ */
+public class StudentJdbcDAOImplTest {
+    @Test
+    public void createStudent() throws Exception {
+
+    }
+
+    @Test
+    public void getStudent() throws Exception {
+
+    }
+
+    @Test
+    public void deleteStudent() throws Exception {
+
+    }
+
+    @Test
+    public void updateStudent() throws Exception {
+
+    }
+
+    @Test
+    public void listStudents() throws Exception {
+
+    }
+
+}
