a9b0ff7d67cccb91c3c43f5e75d63b13364a8576
Roman Sergienko <r-sergienko@ya.ru>
2017-05-31 22:53:26 +0400

    Add @RunWith

--- src/test/java/me/sergienko/dao/StudentJdbcDAOImplTest.java
+++ src/test/java/me/sergienko/dao/StudentJdbcDAOImplTest.java
@@ -1,12 +1,15 @@
 package me.sergienko.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Роман on 30.05.2017.
  */
+@RunWith(BlockJUnit4ClassRunner.class)
 public class StudentJdbcDAOImplTest {
     @Test
     public void createStudent() throws Exception {
