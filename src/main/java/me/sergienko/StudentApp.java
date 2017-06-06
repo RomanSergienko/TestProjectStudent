df4c5a4eb46cbfd76caafaff281a21d5fed75c0b
Roman Sergienko <r-sergienko@ya.ru>
2017-06-06 22:34:35 +0400

    Output list of student to console

--- src/main/java/me/sergienko/StudentApp.java
+++ src/main/java/me/sergienko/StudentApp.java
@@ -1,15 +1,21 @@
 package me.sergienko;
 
 
-import me.sergienko.config.BeenConfig;
+import me.sergienko.config.BeanConfig;
 import me.sergienko.dao.StudentJdbcDAOImpl;
+import me.sergienko.model.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+
 public class StudentApp {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeenConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         StudentJdbcDAOImpl bean = context.getBean(StudentJdbcDAOImpl.class);
-        System.out.println(bean.getStudent(1));
+        List<Student> list = bean.listStudents();
+        for (Student student:list){
+            System.out.println(student);
+        }
     }
 }
