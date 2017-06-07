23479756827706514bd5556f3a9d0a6e188df62e
Executor <r-sergienko@ya.ru>
2017-06-07 10:51:35 +0400

    Add output list of student in console

--- src/main/java/me/sergienko/StudentApp.java
+++ src/main/java/me/sergienko/StudentApp.java
@@ -1,15 +1,21 @@
 package me.sergienko;
 
 
-import me.sergienko.config.BeenConfig;
+import me.sergienko.config.BeanConfig;
 import me.sergienko.dao.StudentJdbcDAOImpl;
+import me.sergienko.model.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+
 public class StudentApp {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(BeenConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
         StudentJdbcDAOImpl bean = context.getBean(StudentJdbcDAOImpl.class);
-        System.out.println(bean.getStudent(1));
+        List<Student> students = bean.listStudents();
+        for (Student st : students) {
+            System.out.println(st);
+        }
     }
 }
