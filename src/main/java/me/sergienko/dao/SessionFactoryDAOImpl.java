cc25529f2bfd30680bf0b1fe2e3055fe0a941433
Executor <r-sergienko@ya.ru>
2017-06-09 10:01:44 +0400

    Add transactional to method Use CriteriaApi for get list of student

--- src/main/java/me/sergienko/dao/SessionFactoryDAOImpl.java
+++ src/main/java/me/sergienko/dao/SessionFactoryDAOImpl.java
@@ -4,14 +4,17 @@ package me.sergienko.dao;
 import me.sergienko.model.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
 public class SessionFactoryDAOImpl implements StudentDAO {
 
-
+    @Autowired
     private SessionFactory sessionFactory;
 
     public void setSessionFactory(SessionFactory sessionFactory) {
@@ -19,6 +22,7 @@ public class SessionFactoryDAOImpl implements StudentDAO {
     }
 
     @Override
+    @Transactional
     public Integer createStudent(Student student) {
         Session session = this.sessionFactory.getCurrentSession();
         int id = (int) session.save(student);
@@ -28,12 +32,14 @@ public class SessionFactoryDAOImpl implements StudentDAO {
     }
 
     @Override
+    @Transactional
     public Student getStudent(Integer id) {
         Session session = this.sessionFactory.getCurrentSession();
         return session.load(Student.class, id);
     }
 
     @Override
+    @Transactional
     public void deleteStudent(Integer id) {
         Session session = this.sessionFactory.getCurrentSession();
         Student student = session.load(Student.class, id);
@@ -43,14 +49,19 @@ public class SessionFactoryDAOImpl implements StudentDAO {
     }
 
     @Override
+    @Transactional
     public void updateStudent(Student student) {
         Session session = this.sessionFactory.getCurrentSession();
         session.update(student);
     }
 
     @Override
+    @Transactional
     public List<Student> listStudents() {
         Session session = this.sessionFactory.getCurrentSession();
-        return session.createQuery("from students").list();
+        CriteriaQuery<Student> criteriaQuery = session.getCriteriaBuilder().createQuery(Student.class);
+        criteriaQuery.from(Student.class);
+        List<Student> studentsList = session.createQuery(criteriaQuery).getResultList();
+        return studentsList;
     }
 }
