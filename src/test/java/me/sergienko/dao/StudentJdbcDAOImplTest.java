db79bb4b790726307165c9d82938ec38fd67c2d0
Roman Sergienko <r-sergienko@ya.ru>
2017-06-11 19:01:05 +0400

    Optimize import

--- src/test/java/me/sergienko/dao/StudentJdbcDAOImplTest.java
+++ src/test/java/me/sergienko/dao/StudentJdbcDAOImplTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.sql.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
