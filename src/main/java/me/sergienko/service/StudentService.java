4ff3fc69ef97f3a30b35e4e3c70fd187742ea13f
Roman Sergienko <r-sergienko@ya.ru>
2017-06-12 19:14:23 +0400

    Add interface StudentService

---
+++ src/main/java/me/sergienko/service/StudentService.java
@@ -0,0 +1,17 @@
+package me.sergienko.service;
+
+import me.sergienko.model.Student;
+
+import java.util.List;
+
+interface StudentService {
+    Integer createStudent(Student student);
+
+    Student getStudent(Integer id);
+
+    void deleteStudent(Integer id);
+
+    void updateStudent(Student student);
+
+    List<Student> listStudents();
+}
