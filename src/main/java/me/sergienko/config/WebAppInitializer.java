e6855580c014722b89774e2c6a6e66f3ddfbc574
Roman Sergienko <r-sergienko@ya.ru>
2017-06-21 21:40:17 +0400

    Add springMvc configuration and initializer

---
+++ src/main/java/me/sergienko/config/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package me.sergienko.config;
+
+
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer {
+    public void onStartup (ServletContext container){
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(BeanConfig.class);
+
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
+        dispatcherServlet.register(MvcConfig.class);
+
+        ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(dispatcherServlet));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+
+    }
+}
