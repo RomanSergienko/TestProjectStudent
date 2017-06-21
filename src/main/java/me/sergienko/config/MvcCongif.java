0d66352f8af73890d5fdc9f9743e47880ee188aa
Roman Sergienko <r-sergienko@ya.ru>
2017-06-21 20:03:30 +0400

    Add config file for SpringMVC

---
+++ src/main/java/me/sergienko/config/MvcCongif.java
@@ -0,0 +1,14 @@
+package me.sergienko.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"me.sergienko.controller"})
+public class MvcCongif extends WebMvcConfigurerAdapter{
+
+}
