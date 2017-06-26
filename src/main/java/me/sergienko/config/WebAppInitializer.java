f37718a715220cf3befd4757a81789b94398e719
Roman Sergienko <r-sergienko@ya.ru>
2017-06-26 20:36:05 +0400

    implements WebApplicationInitializer

--- src/main/java/me/sergienko/config/WebAppInitializer.java
+++ src/main/java/me/sergienko/config/WebAppInitializer.java
@@ -1,6 +1,7 @@
 package me.sergienko.config;
 
 
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
-public class WebAppInitializer {
+public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup (ServletContext container){
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(BeanConfig.class);
