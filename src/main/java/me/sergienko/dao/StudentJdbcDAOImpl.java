17686286c91f963401a63e496f0a65cb59f09c91
Roman Sergienko <r-sergienko@ya.ru>
2017-06-26 20:43:56 +0400

    Add autowiring for constructor

--- src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
+++ src/main/java/me/sergienko/dao/StudentJdbcDAOImpl.java
@@ -1,6 +1,7 @@
 package me.sergienko.dao;
 
 import me.sergienko.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -14,7 +15,7 @@ public class StudentJdbcDAOImpl implements StudentDAO {
 
     private DataSource dataSource;
 
-
+    @Autowired
     public StudentJdbcDAOImpl(DataSource dataSource) {
 
         this.dataSource = dataSource;
