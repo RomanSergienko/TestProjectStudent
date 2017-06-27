c0f2aa0f99915b42cf7e7f959decdd775e6a796a
Roman Sergienko <r-sergienko@ya.ru>
2017-06-27 16:47:53 +0400

    Add handling request studentList

--- src/main/java/me/sergienko/controller/StudentController.java
+++ src/main/java/me/sergienko/controller/StudentController.java
@@ -1,5 +1,25 @@
 package me.sergienko.controller;
 
+import me.sergienko.model.Student;
+import me.sergienko.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-public class StudentController{
+@Controller
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+
+    @RequestMapping(value = "students", method = RequestMethod.GET)
+    public String listBooks(Model model){
+        model.addAttribute("student", new Student());
+        model.addAttribute("listStudents", this.studentService.listStudents());
+
+        return "students";
+    }
 }
